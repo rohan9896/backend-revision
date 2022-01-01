6563defac71eb27773713fe04b350ca0b96041de
Rohan Gupta <rohang9896@gmail.com>
2022-01-01 18:24:47 +0530

    db connection done

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("db connected");
+}).catch((e) => {
+    console.log(e.toString());
+})
 
 app.get('/', (req, res) => {
     res.send('Hello Express app!')
@@ -9,4 +22,3 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('server started');
 });
-  
